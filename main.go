a28d9c46fd7394428fcef19479a08bf733562b07
Derek Tamsen <derek@withpersona.com>
2024-10-20 23:20:38 -0700

    write manifests to directories based on cluster, app, and resource type

--- main.go
+++ main.go
@@ -1,29 +1,58 @@
 package main
 
 import (
+	"example.com/cdk8s/imports/k8s"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
 )
 
-type MyChartProps struct {
-	cdk8s.ChartProps
-}
+func NewChart(scope constructs.Construct, id string, ns string, appLabel string) cdk8s.Chart {
 
-func NewMyChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
-	var cprops cdk8s.ChartProps
-	if props != nil {
-		cprops = props.ChartProps
+	chart := cdk8s.NewChart(scope, jsii.String(id), &cdk8s.ChartProps{
+		Namespace: jsii.String(ns),
+	})
+
+	labels := map[string]*string{
+		"app": jsii.String(appLabel),
 	}
-	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
 
-	// define resources here
+	k8s.NewKubeDeployment(chart, jsii.String("deployment"), &k8s.KubeDeploymentProps{
+		Spec: &k8s.DeploymentSpec{
+			Replicas: jsii.Number(3),
+			Selector: &k8s.LabelSelector{
+				MatchLabels: &labels,
+			},
+			Template: &k8s.PodTemplateSpec{
+				Metadata: &k8s.ObjectMeta{
+					Labels: &labels,
+				},
+				Spec: &k8s.PodSpec{
+					Containers: &[]*k8s.Container{{
+						Name:  jsii.String("app-container"),
+						Image: jsii.String("nginx:1.19.10"),
+						Ports: &[]*k8s.ContainerPort{{
+							ContainerPort: jsii.Number(80),
+						}},
+					}},
+				},
+			},
+		},
+	})
 
 	return chart
 }
 
 func main() {
-	app := cdk8s.NewApp(nil)
-	NewMyChart(app, "cdk8s", nil)
+	appProps := &cdk8s.AppProps{
+		Outdir:              jsii.String("dist/cluster"),
+		OutputFileExtension: jsii.String(".yaml"),
+		YamlOutputType:      cdk8s.YamlOutputType_FOLDER_PER_CHART_FILE_PER_RESOURCE,
+	}
+
+	app := cdk8s.NewApp(appProps)
+
+	NewChart(app, "exemplar", "my-app", "my-app")
+
 	app.Synth()
 }
